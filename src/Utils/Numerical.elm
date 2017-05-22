fed4580de19e6280734e99cc8b3eeac5e0d2e09c
paulchertov <paulchertov@gmail.com>
2017-05-22 13:59:16 +0300

    added util that will be used in Bar

---
+++ src/Utils/Numerical.elm
@@ -0,0 +1,15 @@
+module Utils.Numerical exposing(maxPowerOfTwo)
+
+--returns maximal power of two, for which power of two is less than limit
+--not tested--
+maxPowerOfTwo: Int -> Int
+maxPowerOfTwo limit =
+  let
+    maxPowerOfTwo_: Int -> Maybe Int
+    maxPowerOfTwo_ current =
+      if 2^current > limit then Nothing
+      else case maxPowerOfTwo_ (current + 1) of
+        Nothing -> Just current
+        Just val -> Just val
+  in
+    maxPowerOfTwo_ 1
