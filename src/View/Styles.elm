e861415c172bc9c6e92df2da7fa053d116ef0d22
paulchertov <paulchertov@gmail.com>
2017-06-19 21:17:35 +0300

    fixed Styles, added style to view

--- src/View/Styles.elm
+++ src/View/Styles.elm
@@ -1,12 +1,20 @@
-module Style exposing (style_tag)
+module View.Styles exposing (style_tag)
 import Html exposing (text, node)
-import Html.Attributes exposing (class, type')
+import Html.Attributes exposing (class, type_)
 
 style_text = """
-#elm-guitar .bar .tab-bar .string{
-    margin: 0px;
-    line-height: 0.5;
+#elm-guitar .bar .chord {
+  float:left;
+}
+
+#elm-guitar .bar .tab-bar .chord .string{
+  margin: 0px;
+  line-height: 0.5;
+}
+
+#elm-guitar .bar .note-bar .chord .string{
+  line-height: 0.5;
 }
 """
 style_tag =
-    node "style" [type' "text/css"] [text style_text]
+  node "style" [type_ "text/css"] [text style_text]
--- src/View/View.elm
+++ src/View/View.elm
@@ -4,10 +4,12 @@ import Array
 import Html.Attributes exposing (id)
 import Html exposing (div)
 
+import View.Styles exposing (style_tag)
 import View.Bar exposing (mapBar)
 
 view model =
   div
     [id "elm-guitar"]
+    <| List.append [style_tag]
     <| Array.toList
     <| Array.indexedMap (mapBar model) model.bars
